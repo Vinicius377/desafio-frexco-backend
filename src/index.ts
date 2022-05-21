44ae382c1beaac2ced17d8343579ee3de0064bbc
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2022-05-21 14:31:08 -0300

    add sqlite database

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import routes from './routes'
+import { openDb } from './database/sqliteConfig'
 
+const PORT = process.env.PORT || 2000
 const app = express()
 
+openDb()
 app.use(express.json())
 app.use(routes)
 
-app.listen(2000, () => {
-  console.log('server is running at port ')
+app.listen(PORT, () => {
+  console.log('server is running at port ' + PORT)
 })
