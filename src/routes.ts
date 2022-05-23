b005faa30ed2ff0413be3fe62814e70ea662f114
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2022-05-23 14:37:29 -0300

    add routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,21 @@
 import { Router } from 'express'
+import Authenticate from './middleware/EnsureAuthenticate'
 
+import createProduct from './controllers/createProduct'
+import deleteProduct from './controllers/deleteProduct'
+import updateProduct from './controllers/updateProduct'
+import getProduct from './controllers/getProducts'
+
+import authenticateLogin from './controllers/authenticateLogin'
+import createAccount from './controllers/createAccount'
 const routes = Router()
+
+routes.get('/product', getProduct)
+routes.put('/product', Authenticate, updateProduct)
+routes.delete('/product', Authenticate, deleteProduct)
+routes.post('/product', Authenticate, createProduct)
+
+routes.post('/login', authenticateLogin)
+routes.post('/user', createAccount)
 
 export default routes
